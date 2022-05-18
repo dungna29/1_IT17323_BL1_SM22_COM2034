91a436d8d5e8bb560dc72c8f4b70041c5ee4ebd3
Dungna89 <dungna29@fpt.edu.vn>
2022-05-18 13:54:28 +0700

    1

---
+++ 1_IT17323_BL1_SM22_COM2034_FILE_CODE.sql
@@ -0,0 +1,10 @@
+﻿-- SQLQuery_NangCao_DungNA29_IT17202_SP22_BL1
+-- Các phím tắt cơ bản:
+-- Ctrl + /: Dùng comment code
+-- F5: Dùng để chạy câu lệnh SQL
+
+-- Sử dụng SQL: 
+-- Chạy câu lệnh SQL đang được chọn (Ctrl + E)
+-- Chuyển câu lệnh đang chọn thành chữ hoa, chữ thường (Ctrl + Shift + U, Ctrl + Shift + L)
+-- Comment và bỏ comment dòng lệnh ( Ctrl + K + C; Ctrl + K + U)
+
